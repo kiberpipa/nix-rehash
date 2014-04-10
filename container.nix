731de7e1d23536311e9e26b871e9bcdccd074827
Jaka Hudoklin <jakahudoklin@gmail.com>
2014-04-10 11:27:08 +0200

    container: WIP user lxc for virtualization

--- container.nix
+++ container.nix
@@ -32,18 +32,68 @@ let
     prefix = [ "systemd" "containers" name ];
   }).config.system.build.toplevel;
 
+  containerConfig = ''
+    lxc.utsname = ${name}
+    lxc.arch = ${if system == "x86_64-linux" then "x86_64" else "i686"}
+    lxc.tty = 6
+    lxc.pts = 1024
+
+    ## Capabilities
+    lxc.cap.drop = audit_control audit_write mac_admin mac_override mknod setfcap
+    lxc.cap.drop = sys_boot sys_module sys_pacct sys_rawio sys_time
+
+    ## Devices
+    lxc.cgroup.devices.deny = a # Deny access to all devices
+
+    # Allow to mknod all devices (but not using them)
+    lxc.cgroup.devices.allow = c *:* m
+    lxc.cgroup.devices.allow = b *:* m
+
+    lxc.cgroup.devices.allow = c 1:3 rwm
+    lxc.cgroup.devices.allow = c 1:5 rwm
+    lxc.cgroup.devices.allow = c 1:8 rwm
+    lxc.cgroup.devices.allow = c 1:9 rwm
+    lxc.cgroup.devices.allow = c 4:0 rwm
+    lxc.cgroup.devices.allow = c 4:1 rwm
+    lxc.cgroup.devices.allow = c 4:2 rwm
+    lxc.cgroup.devices.allow = c 4:3 rwm
+    lxc.cgroup.devices.allow = c 5:0 rwm
+    lxc.cgroup.devices.allow = c 5:1 rwm
+    lxc.cgroup.devices.allow = c 5:2 rwm
+    lxc.cgroup.devices.allow = c 10:229 rwm
+    lxc.cgroup.devices.allow = c 136:* rwm
+    lxc.cgroup.devices.allow = c 254:0 rwm
+
+    ## Mounts
+    lxc.mount.auto = proc sys cgroup
+    lxc.mount.entry = /nix/store nix/store none defaults,bind.ro 0.0
+    lxc.autodev = 1
+
+    ## Network
+    lxc.network.type = veth
+    lxc.network.name = eth0
+    lxc.network.flags = up
+    '';
+
   startContainer = writeTextFile {
     name = "${name}-container-start";
     text = ''
       #!${pkgs.bash}/bin/bash
-      mkdir -p -m 0755 ${container_root}/etc
-      mkdir -p /var/log/containers
-      if ! [ -e ${container_root}/etc/os-release ]; then
-        touch ${container_root}/etc/os-release
+      if [ -z "$CONTAINER_ROOT" ]; then
+          export CONTAINER_ROOT="/var/lib/containers/${name}"
       fi
-      rm ${container_root}/old_config && ln -fs ${container} ${container_root}/old_config
-      rm ${container_root}/new_config && ln -fs ${container} ${container_root}/new_config
-      nohup ${pkgs.systemd}/bin/systemd-nspawn -M ${name} -D ${container_root} --bind-ro=/nix ${container}/init > /var/log/containers/${name}.log &
+      echo "Using $CONTAINER_ROOT as rootfs"
+      mkdir -p $CONTAINER_ROOT/{proc,sys,dev,nix/store,etc}
+      chmod 0755 $CONTAINER_ROOT/etc
+      if ! [ -e $CONTAINER_ROOT/etc/os-release ]; then
+        touch $CONTAINER_ROOT/etc/os-release
+      fi
+      rm $CONTAINER_ROOT/old_config && ln -fs "${container}" $CONTAINER_ROOT/old_config
+      rm $CONTAINER_ROOT/new_config && ln -fs "${container}" $CONTAINER_ROOT/new_config
+      ${pkgs.lxc}/bin/lxc-start -n "${name}" \
+        -f "${pkgs.writeText "container.conf" containerConfig}" \
+        -s lxc.rootfs=$CONTAINER_ROOT \
+        "$@" "${container}/init"
     '';
     executable = true;
     destination = "/bin/${name}-container-start";
@@ -53,20 +103,7 @@ let
     name = "${name}-container-stop";
     text = ''
       #!${pkgs.bash}/bin/bash
-      pid="$(cat /sys/fs/cgroup/systemd/machine/${name}.nspawn/system/tasks 2> /dev/null)"
-      if [ -n "$pid" ]; then
-          # Send the RTMIN+3 signal, which causes the container
-          # systemd to start halt.target.
-          echo "killing container systemd, PID = $pid"
-          kill -RTMIN+3 $pid
-          # Wait for the container to exit.  We can't let systemd
-          # do this because it will send a signal to the entire
-          # cgroup.
-          for ((n = 0; n < 180; n++)); do
-          if ! kill -0 $pid 2> /dev/null; then break; fi
-          sleep 1
-          done
-      fi
+      ${pkgs.lxc}/bin/lxc-stop -n "${name}" "$@"
     '';
     executable = true;
     destination = "/bin/${name}-container-stop";
@@ -76,7 +113,10 @@ let
     name = "${name}-container-update";
     text = ''
       #!${pkgs.bash}/bin/bash
-      rm ${container_root}/new_config && ln -fs ${container} ${container_root}/new_config
+      if [ -z "$CONTAINER_ROOT" ]; then
+          export CONTAINER_ROOT="/var/lib/containers/${name}"
+      fi
+      rm $CONTAINER_ROOT/new_config && ln -fs ${container} $CONTAINER_ROOT/new_config
     '';
     executable = true;
     destination = "/bin/${name}-container-update";
