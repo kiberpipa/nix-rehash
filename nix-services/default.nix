0aff77492571d8042ff57a4926ee0c97999df5d6
Jaka Hudoklin <jakahudoklin@gmail.com>
2014-09-23 14:32:36 +0200

    services: add influxdb service

--- nix-services/default.nix
+++ nix-services/default.nix
@@ -31,6 +31,7 @@ let
     <nixpkgs/nixos/modules/services/databases/mongodb.nix>
     <nixpkgs/nixos/modules/services/databases/couchdb.nix>
     <nixpkgs/nixos/modules/services/databases/neo4j.nix> 
+    <nixpkgs/nixos/modules/services/databases/influxdb.nix> 
     <nixpkgs/nixos/modules/services/search/elasticsearch.nix>
     <nixpkgs/nixos/modules/services/monitoring/graphite.nix>
     <nixpkgs/nixos/modules/services/monitoring/statsd.nix>
